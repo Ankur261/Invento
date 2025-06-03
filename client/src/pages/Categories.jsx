b1ea4c81656ca66b4d84fcb68face4f6d47b4746
Mansi Patil <mansip334@gmail.com>
2025-06-03 22:32:45 +0530

    Add test

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,4 +1,7 @@
 const Categories = () => {
+  <h2 className="text-xl font-semibold text-gray-800 mb-4">Add New Category</h2>
+
+
   return <h1 className="text-2xl font-bold text-red-950">Categories Page</h1>;
 };
 
